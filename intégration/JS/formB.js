2e1fe56f30bf214d7c21fc7280e7a8108562e8eb
quentin-j <quentin678jougla@gmail.com>
2021-12-17 14:36:50 +0100

    correctif bug js

--- intégration/JS/formB.js
+++ intégration/JS/formB.js
@@ -1,11 +1,12 @@
+
 const list = document.querySelector('.list');
 const form = document.querySelector('form');
-const title = document.querySelector('.title');
+const spanTitle = document.querySelector('.spanTitle');
 const item = document.querySelector('.item');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    list.innerHTML += `<li><span class="titleForm">${title.value} </span>${item.value}</li>`;
-    title.value = '';
+    list.innerHTML += `<li><span class="titleForm">${spanTitle.value}</span> ${item.value}</li>`;
+    spanTitle.value = '';
     item.value = '';
 });
