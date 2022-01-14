b45515757eaf0a165c16a62167f81ba7e68fef98
quentin-j <quentin678jougla@gmail.com>
2022-01-14 12:07:54 +0100

    restart symfo

--- zombieTales/public/JS/form.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector('.list');
-const form = document.querySelector('form');
-const item = document.querySelector('.item');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    list.innerHTML += `<li>${item.value}</li>`;
-    item.value = '';
-});
